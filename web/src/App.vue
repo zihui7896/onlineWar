bc636702e2805b512c076a161016ea4b069ea010
zihui <3335914015@qq.com>
2022-10-26 08:18:23 +0800

    前端 从 后端 获取数据name rating

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,38 @@
 <template>
   <div>
     hello world
+    <div>Bot 昵称 : {{ bot_name }}</div>
+    <div>Bot 战力 : {{ bot_rating }}</div>
   </div>
   <router-view></router-view>
 </template>
+
+<script>
+import $ from 'jquery';
+import { ref } from 'vue';
+
+export default {
+  name: "APP",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+    
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",
+      type: "get",
+      success: resp => { 
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      }
+    })
+    return {
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
 
 <style>
 </style>
