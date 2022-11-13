9d8c9a17b3fc134ffca0e1063feb0ca88bed19e7
zihui <3335914015@qq.com>
2022-11-13 22:23:51 +0800

    实现后端api add remove update getlist

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -6,10 +6,80 @@
 
 <script>
 import ContentField from "@/components/ContentField.vue"
-
+import $ from 'jquery';
+import { useStore } from "vuex";
 export default {
     components: {
         ContentField
+    },
+    setup() {
+        const store = useStore();
+        // $.ajax ({ // add bot
+        //       url: "http://127.0.0.1:3000/user/bot/add/",
+        //       type: "post",
+        //       data: {
+        //         title: "我的bot",
+        //         description: "bot描述",
+        //         content: "代码",
+        //       },
+        //       headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //       },
+        //       success(resp) {
+        //         console.log(resp);
+        //       },
+        //       errot(resp) {
+        //         console.log(resp);
+        //       }
+        // });
+        // $.ajax ({ // 删除bot
+        //       url: "http://127.0.0.1:3000/user/bot/remove/",
+        //       type: "post",
+        //       data: {
+        //         bot_id: 7,
+        //       },
+        //       headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //       },
+        //       success(resp) {
+        //         console.log(resp);
+        //       },
+        //       errot(resp) {
+        //         console.log(resp);
+        //       }
+        // });
+        // $.ajax ({ // update bot
+        //       url: "http://127.0.0.1:3000/user/bot/update/",
+        //       type: "post",
+        //       data: {
+        //         bot_id: 4,
+        //         title: "我的bot88",
+        //         description: "bot描述88",
+        //         content: "代码88",
+        //       },
+        //       headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //       },
+        //       success(resp) {
+        //         console.log(resp);
+        //       },
+        //       errot(resp) {
+        //         console.log(resp);
+        //       }
+        // });
+        // $.ajax ({ // select bot
+        //       url: "http://127.0.0.1:3000/user/bot/getlist/",
+        //       type: "get",
+        //       headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //       },
+        //       success(resp) {
+        //         console.log(resp);
+        //       },
+        //       errot(resp) {
+        //         console.log(resp);
+        //       }
+        // });
     }
 }
 </script>
